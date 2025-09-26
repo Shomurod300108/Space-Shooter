7f1a7aa9ee9dff9811f0c11f438e6a35702e5756
Shomurod300108 <your_email@example.com>
2025-09-27 00:09:45 +0500

    Space shooter project is created

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Text _scoreText;
+    //handle to Text
+    void Start()
+    {
+        //assign text component to the handle
+        _scoreText.text = "Score:" + 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
